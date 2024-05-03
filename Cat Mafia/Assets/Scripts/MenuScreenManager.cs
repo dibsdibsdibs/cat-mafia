8b84bcd7e64c7f192fe7abf753d4ed3f320c622e
Divine <divinelavente@gmail.com>
2024-05-03 16:00:37 +0800

    included first scene movement

--- Cat Mafia/Assets/Scripts/MenuScreenManager.cs
+++ Cat Mafia/Assets/Scripts/MenuScreenManager.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class MenuScreenManager : MonoBehaviour
 {
-    [SerializeField] public GameObject CatCharacter;
+    [SerializeField] public GameObject catCharacter;
     [SerializeField] public Vector3 targetPosition;
     [SerializeField] public float speed = 5f;
 
     void Start()
     {
-        transform.position = Vector3.Lerp(transform.position, targetPosition, speed * Time.deltaTime);
+        FirstScene();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    void FirstScene()
+    {
+        Vector3 targetPosition = catCharacter.transform.position + Vector3.right * 5f;
+
+        StartCoroutine(MoveToTargetPosition(targetPosition));
+    }
+
+    IEnumerator MoveToTargetPosition(Vector3 targetPosition)
+    {
+        while (catCharacter.transform.position != targetPosition)
+        {
+            // Move the GameObject towards the target position
+            catCharacter.transform.position = Vector3.MoveTowards(catCharacter.transform.position, targetPosition, speed * Time.deltaTime);
+            yield return null;
+        }
     }
 }
