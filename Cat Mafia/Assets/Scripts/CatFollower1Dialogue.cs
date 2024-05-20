5cad65df89d636547c21dd9e97d1d11f371df8a2
Jewynah <jhjaronay@gmail.com>
2024-05-20 22:39:57 +0800

    Revert "bug-fix/fixed-cat-movement"
    This reverts commit 4345ea228bc49e7ae9619054906426af42487707.

--- Cat Mafia/Assets/Scripts/CatFollower1Dialogue.cs
+++ Cat Mafia/Assets/Scripts/CatFollower1Dialogue.cs
@@ -7,63 +7,33 @@ using UnityEngine;
 
 public class CatFollower1Dialogue : MonoBehaviour
 {
-    private Animator animator;
+    public TextMeshProUGUI interactText;
     public Vector3 targetPosition; 
-    public Vector3 finalTargetPosition;
     public Vector3 startPosition; 
     public GameObject Cat1Dialogue;
     public bool isDone;
-    public bool isInPosition;
     public DialogueManager checkDialogue;
     public float speed = 2f;
 
     void Start()
     {   
-        animator = GetComponent<Animator>();
+    
         Cat1Dialogue.SetActive(false); 
         checkDialogue = Cat1Dialogue.GetComponent<DialogueManager>();
-        transform.position =  startPosition;
-        isInPosition = false;
+        transform.position = startPosition;
     }
 
     void Update()
     {
-        if (!isInPosition)
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        if (transform.position == targetPosition)
         {
-            animator.SetFloat("moveX", -1f);
-            animator.SetFloat("moveY", 0);
-            animator.SetBool("isMoving", true);
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
-            
-            if (Vector3.Distance(transform.position, targetPosition) < 0.01f)
-            {
-                transform.position = targetPosition;
-                isInPosition = true;
-            }
+            Cat1Dialogue.SetActive(true);
         }
-        else
-        {
 
-            animator.SetFloat("moveX", 0);
-            animator.SetFloat("moveY", 1f);
-            animator.SetBool("isMoving", true);
-            transform.position = Vector3.MoveTowards(transform.position, finalTargetPosition, speed * Time.deltaTime);
-            if (Vector3.Distance(transform.position, finalTargetPosition) < 0.01f)
-            {
-                animator.SetFloat("moveX", -1f);
-                animator.SetFloat("moveY", 0);
-                OffIsMoving();
-                transform.position = finalTargetPosition;
-                
-                Cat1Dialogue.SetActive(true);
-            }
-        }
         if (isDone)
         {
             Cat1Dialogue.SetActive(false); 
         }
-    }
-    public void OffIsMoving(){
-        animator.SetBool("isMoving", false);
     }
 }
