13cba16e21ec7a7f7d55f7d77f35ca30bc03adc4
Jewynah <jhjaronay@gmail.com>
2024-05-20 16:45:18 +0800

    bug-fix/move-automatic
    No need to move the main player to cat follower 1. It automatically moves now

--- Cat Mafia/Assets/Scripts/CatFollower1Dialogue.cs
+++ Cat Mafia/Assets/Scripts/CatFollower1Dialogue.cs
@@ -2,45 +2,38 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor;
 using UnityEngine;
 
 public class CatFollower1Dialogue : MonoBehaviour
 {
     public TextMeshProUGUI interactText;
     public Vector3 targetPosition; 
+    public Vector3 startPosition; 
     public GameObject Cat1Dialogue;
-    public float margin = 1f;
     public bool isDone;
-    private bool isInPosition;
     public DialogueManager checkDialogue;
+    public float speed = 2f;
 
     void Start()
     {   
-        interactText.enabled = false;
+    
         Cat1Dialogue.SetActive(false); 
         checkDialogue = Cat1Dialogue.GetComponent<DialogueManager>();
-        isInPosition = false;
+        transform.position = startPosition;
     }
 
     void Update()
     {
-        if (!isInPosition && Vector3.Distance(transform.position, targetPosition) < margin)
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        if (transform.position == targetPosition)
         {
-            isInPosition = true;
-            interactText.enabled = true;
-        }
-
-        if (isInPosition && Input.GetKeyDown(KeyCode.Z))
-        {
-            interactText.enabled = false; 
             Cat1Dialogue.SetActive(true);
         }
 
         if (isDone)
         {
             Cat1Dialogue.SetActive(false); 
-            isDone = false;
-            isInPosition = false;
         }
     }
 }
