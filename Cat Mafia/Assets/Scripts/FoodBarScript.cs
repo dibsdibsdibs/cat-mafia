669f6fb29380673f6433b1e2864e97f84587d058
Divine <divinelavente@gmail.com>
2024-05-25 14:18:32 +0800

    food bar script

---
+++ Cat Mafia/Assets/Scripts/FoodBarScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FoodBarScript : MonoBehaviour
+{
+    private Slider slider;
+    [SerializeField] public float sliderValue;
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    void Update()
+    {
+        slider.value = sliderValue;
+    }
+
+    void UpdateBar(float itemValue)
+    {
+        sliderValue = slider.value + itemValue;
+        Debug.Log("Food bar value updated");
+    }
+}
